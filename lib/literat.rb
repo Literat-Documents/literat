1c1210ff050add4a93bb234d529b3ea82b7c0eeb
NWHirschfeld <Niclas@NWHirschfeld.de>
2018-10-07 00:32:36 +0200

    added feature to load template from git repo
    Signed-off-by: NWHirschfeld <Niclas@NWHirschfeld.de>

--- lib/literat.rb
+++ lib/literat.rb
@@ -63,11 +63,27 @@ Literat.define_command('list') do
 
   run do |opts, args, cmd|
   	template_manager = TemplateManager.new
-	puts 'TEMPLATES'.light_red.bold
-	puts
-	template_manager.list.each do |template|
-		puts "\t#{template['name'].green}\t#{template['description']}"
-	end
-	puts
+  	puts 'TEMPLATES'.light_red.bold
+  	puts
+  	template_manager.list.each do |template|
+  		puts "\t#{template['name'].green}\t#{template['description']}"
+  	end
+  	puts
+  end
+end
+
+Literat.define_command('gitadd') do
+  name        'gitadd'
+  usage       "gitadd [options]"
+  summary     'add template from git repo'
+
+  option :u,  :url, 'URL', argument: :required
+  run do |opts, args, cmd|
+    if opts[:url].nil?
+      puts cmd.help
+      exit 0
+    end
+    template_manager = TemplateManager.new
+    template_manager.add_from_git opts[:url]
   end
 end
--- lib/literat/template_manager.rb
+++ lib/literat/template_manager.rb
@@ -1,11 +1,16 @@
+require 'securerandom' # random Strings for git clone folder
+require 'git' # git wrapper
+
 class TemplateManager
   TemplateDirectory="#{Dir.home}/.literat/templates/"
+  TemporyDirectory='/tmp/'
+  TemplateConfigFile='template.yml'
 
   def list
-	Dir.entries(TemplateDirectory)[2..-1].map do |folder|  
-	  template = Template.new folder
-	  template.config
-	end
+  	Dir.entries(TemplateDirectory)[2..-1].map do |folder|  
+  	  template = Template.new folder
+  	  template.config
+  	end
   end
 
   def check_init
@@ -29,5 +34,28 @@ class TemplateManager
 
   def exists? name
   	File.directory?("#{TemplateDirectory}/#{name}")
+  end
+
+  def add_from_git url
+    temp_folder = "literat_temp_git_#{SecureRandom.hex}"
+    full_temp_folder = "#{TemporyDirectory}/#{temp_folder}"
+    # git clone template
+    if Git.clone(url, temp_folder, :path => TemporyDirectory).nil?
+      # TODO: error
+      exit
+    end
+
+    template_name = YAML.load_file("#{full_temp_folder}/#{TemplateConfigFile}")['name']
+
+    # git check for name conflict
+    if (list.map { |e| e['name'] }.include? template_name)
+      # TODO: error template with this name already exists
+      puts "already have that template"
+    else
+      # copy to template directory and rename it
+      # TODO: sanitize name of new folder
+      FileUtils.copy_entry full_temp_folder, "#{TemplateDirectory}/#{template_name}"
+      # TODO: delete temporary Folder
+    end
   end
 end
